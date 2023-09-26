986c412260fcd2dae165e64ab38a6ba87050d4d9
ejtang <a123451121@gmail.com>
2023-09-26 15:32:46 +0800

    act

--- src/__test__/InputComponent.test.js
+++ src/__test__/InputComponent.test.js
@@ -1,16 +1,22 @@
 import userEvent from '@testing-library/user-event';
 import InputComponent from '../InputComponent';
 import { fireEvent, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 
 test('input change correct', async () => {
     const user = userEvent.setup()
 
-    render(<InputComponent />)
+    // 建議需要包進act裡
+    act(() => { render(<InputComponent />) })
+
     const textbox = screen.getByRole('textbox');
 
     // invoke input onChange
-    // fireEvent.change(textbox, { target: { value: 'test' } });
-    await user.type(textbox, 'World!');
+    // fireEvent.change(textbox, { target: { value: 'World!' } });
+    // await user.type(textbox, 'World!');
+    // 這邊需要包進act裡面
+    await act(async () => { await user.type(textbox, 'World!'); })
+
 
     expect(textbox.value).toBe('World!');
     screen.debug();
